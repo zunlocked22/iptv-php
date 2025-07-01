3e33315c9ca8668b2b7ba7424b6de03fd1bf70dc
zunlocked22 <a2zunlockedver2@gmail.com>
2025-07-01 18:25:47 +0800

    key.js

--- key.js
+++ key.js
@@ -1,21 +1,16 @@
 module.exports = (req, res) => {
   const ua = req.headers['user-agent'] || '';
-
-  const allowedAgents = [
-    'OTT Navigator',
-    'ExoPlayer',
-    'Dalvik',
-    'IPTV',
-    'TV',
-    'SmartTV'
-  ];
+  const allowedAgents = ['OTT Navigator', 'ExoPlayer', 'Dalvik', 'IPTV', 'TV', 'SmartTV'];
 
   if (!allowedAgents.some(agent => ua.includes(agent))) {
     return res.status(403).send('Access Denied');
   }
 
   const keys = {
-    spotv: 'ec7ee27d83764e4b845c48cca31c8eef:9c0e4191203fccb0fde34ee29999129e',
+    history: 'a7724b7ca2604c33bb2e963a0319968a:6f97e3e2eb2bade626e0281ec01d3675',
+    bbcearth: '34ce95b60c424e169619816c5181aded:0e2a2117d705613542618f58bf26fc8e',
+    fashiontv: '971ebbe2d887476398e97c37e0c5c591:472aa631b1e671070a4bf198f43da0c7',
+    hbosignature: 'a06ca6c275744151895762e0346380f5:559da1b63eec77b5a942018f14d3f56f'
   };
 
   const id = req.query.id;
